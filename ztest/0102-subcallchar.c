cb64f5f273dd5f79512691ec80506940d9ea46f2
zu2 <3023910+zu2@users.noreply.github.com>
2025-01-02 11:29:42 +0900

    A char argument pushes one byte. test it.

---
+++ ztest/0102-subcallchar.c
@@ -0,0 +1,39 @@
+char subc(char x,char y)
+{
+	char	z=3;
+
+	if (x!=1)
+		return 1;
+	if (y!=2)
+		return 2;
+	if (z!=3)
+		return 3;
+
+	return 0;
+}
+void subi(int x,int y)
+{
+	int	z=3;
+
+	if (x!=1)
+		return 11;
+	if (y!=2)
+		return 12;
+	if (z!=3)
+		return 13;
+
+	return 0;
+
+}
+
+int main(int argc, char **argv)
+{
+	int r;
+
+	if(r=subc(1,2))
+		return r;
+	if(r=subi(1,2))
+		return r;
+
+	return 0;
+}
