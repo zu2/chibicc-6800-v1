25d4f7c434a658e62526a3339166c554cdc9b16e
zu2 <3023910+zu2@users.noreply.github.com>
2025-01-14 21:49:29 +0900

    add test program

---
+++ ztest/0701-vla.c
@@ -0,0 +1,21 @@
+#include "common.h"
+
+int sub(int x)
+{
+	unsigned char	i;
+	unsigned char	sum = 0;
+	unsigned char	p[x];
+
+	for (i=0; i<10; i++){
+		p[i] = i;
+	}
+	for (i=0; i<10; i++){
+		sum += p[i];
+	}
+	return sum;
+}
+
+int main(int argc, char **argv)
+{
+	return sub(10)!=45;
+}
