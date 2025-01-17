da8a1bc556a2c02c0248cbbcdf88211821b3e1e1
zu2 <3023910+zu2@users.noreply.github.com>
2025-01-17 15:50:18 +0900

    Speed up processing when TOS is less than 256. Array subscript calculations are slightly faster.

--- libc/__mul16x16.s
+++ libc/__mul16x16.s
@@ -6,22 +6,31 @@
 __mul16x16:
 	stab @tmp1+1
 	staa @tmp1
-	ldab #16
-	stab @tmp2
 	tsx
+	tst 2,x		; TOS < 256 ?
+	bne  __mul16x16_10
+	ldab 3,x
+	stab 2,x
+	clr 3,x
+	ldab #8
+	bra __mul16x16_11
+__mul16x16_10:
+	ldab #16
+__mul16x16_11:
+	stab @tmp2
 	; 0-1,x return address
 	; 2-3,x TOS
 	clra
 	clrb
-__mul16x16_1:
+__mul16x16_50:
 	aslb
 	rola
 	rol 3,x
 	rol 2,x
-	bcc __mul16x16_2
+	bcc __mul16x16_90
 	addb @tmp1+1
 	adca @tmp1
-__mul16x16_2:
+__mul16x16_90:
 	dec @tmp2
-	bne __mul16x16_1
+	bne __mul16x16_50
 	rts
