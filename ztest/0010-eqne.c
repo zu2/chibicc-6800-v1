0b382122205306ddc29c7c7bae59f10dbff1b645
zu2 <3023910+zu2@users.noreply.github.com>
2025-04-09 08:58:21 +0900

    add test pattern.

--- ztest/0010-eqne.c
+++ ztest/0010-eqne.c
@@ -9,5 +9,17 @@ int ne(int a, int b)
 
 int main(int argc, char **argv)
 {
-	return eq(0,1);
+	if(!eq(0,0))
+	  return 1;
+
+	if(eq(0,1))
+	  return 2;
+
+	if(!ne(0,1))
+	  return 3;
+
+	if(ne(1,1))
+	  return 4;
+
+	return 0;
 }
