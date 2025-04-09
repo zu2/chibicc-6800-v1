385e708599593dc55dbae493e96a7271f166867b
zu2 <3023910+zu2@users.noreply.github.com>
2025-04-09 09:13:57 +0900

    test bug fix.

--- ztest/0092-struct-complex.c
+++ ztest/0092-struct-complex.c
@@ -1,3 +1,5 @@
+#include "common.h"
+
 struct complex {
     int r;
     int i;
@@ -14,14 +16,24 @@ void complex_add2(struct complex *com_1, struct complex *com_2, struct complex *
 }
 
 void complex_pluseq(struct complex *com_1, struct complex *com_2, struct complex *com_3){
-    com_1->r += com_2->r + com_3->r;
-    com_1->i += com_2->i + com_3->i;
+    com_3->r += com_1->r + com_2->r;
+    com_3->i += com_1->i + com_2->i;
 }
 
 void complex_pluseq2(struct complex *com_1, struct complex *com_2, struct complex *com_3){
-    (*com_1).r += (*com_2).r + (*com_3).r;
-    (*com_1).i += (*com_2).i + (*com_3).i;
+    (*com_3).r += (*com_1).r + (*com_2).r;
+    (*com_3).i += (*com_1).i + (*com_2).i;
 }
+
+struct complex complex_add3(struct complex *com_1, struct complex *com_2){
+    struct complex com_3;
+
+    com_3.r = (*com_1).r + (*com_2).r;
+    com_3.i = (*com_1).i + (*com_2).i;
+
+    return com_3;
+}
+
 
 main(){
         struct complex com01, com02, com03;
@@ -30,31 +42,47 @@ main(){
         com01.i=3;
         com02.r=4;
         com02.i=8;
+        com03.r=0;
+        com03.i=0;
 
-        complex_add(&com01,&com02,&com03);
+        complex_add(&com01,&com02,&com03); // com03 = com01 + com02
 
 	if (com01.r + com02.r != com03.r)
 		return 1;
 	if (com01.i + com02.i != com03.i)
 		return 2;
 
-        complex_add2(&com01,&com02,&com03);
+        complex_add2(&com01,&com02,&com03); // com03 = com01 + com02
 	if (com01.r + com02.r != com03.r)
 		return 11;
 	if (com01.i + com02.i != com03.i)
 		return 12;
 
-        complex_pluseq(&com01,&com02,&com03);
-	if (com01.r != 12)
+        com03.r=0;
+        com03.i=0;
+        complex_pluseq(&com01,&com02,&com03); // com03 += com01 + com02
+	if (com01.r + com02.r != com03.r)
 		return 21;
-	if (com01.i != 22)
+	if (com01.i + com02.i != com03.i)
 		return 22;
 
-        complex_pluseq2(&com01,&com02,&com03);
-	if (com01.r != 22)
+        com03.r=0;
+        com03.i=0;
+        complex_pluseq2(&com01,&com02,&com03);	// com03 += com01 + com02
+	if (com01.r + com02.r != com03.r)
 		return 31;
-	if (com01.i != 41)
+	if (com01.i + com02.i != com03.i)
 		return 32;
+
+        com03.r=0;
+        com03.i=0;
+        com03 = complex_add3(&com01,&com02);	// com03 = com01 + com02
+	print(com03.r);
+	print(com03.i);
+	if (com01.r + com02.r != com03.r)
+		return 41;
+	if (com01.i + com02.i != com03.i)
+		return 42;
 
 	return 0;
 }
