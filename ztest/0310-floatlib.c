b6e7a319602ded08610639258e5505a28ea79b45
zu2 <3023910+zu2@users.noreply.github.com>
2025-04-16 13:00:10 +0900

    small fix

--- ztest/0310-floatlib.c
+++ ztest/0310-floatlib.c
@@ -7,25 +7,20 @@
 extern	float	fabs(float x);
 
 int
-cmpf(float f, float g)
+cmpfl(float f, unsigned long g)
 {
 	int i;
 	unsigned char *p = (unsigned char *)&f;
 	unsigned char *q = (unsigned char *)&g;
 
-	puthexf(f);putchar(' ');
-	puthexf(g);putchar('\n');
-#if 0
 	for (i=0; i<4; ++i,++p,++q){
 		if (*p != *q){
 			return	i+1;
 		}
 	}
-#endif
+
 	return 0;
 }
-
-
 
 int main(int argc, char **argv)
 {
@@ -38,10 +33,6 @@ int main(int argc, char **argv)
 		return 2;
 	if (f == fabs(0.0))
 		return 3;
-#if	0
-	putstr("1.0f:\t");puthexf(f);putchar('\n');
-	putstr("(unsigned long)1.0f:\t");puthexl((unsigned long)1.0);putchar('\n');
-#endif
 
 	return 0;
 }
