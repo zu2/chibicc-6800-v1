d0294ff57da30fedf1537c00b171cf38c79b55c8
zu2 <3023910+zu2@users.noreply.github.com>
2025-04-19 19:24:04 +0900

    add memset

---
+++ libc/__memset.s
@@ -0,0 +1,57 @@
+;
+;	memset(char *s,int c,size_t n)
+;	{
+;	  char *p = s;
+;	  while (n--){
+;	    *p++ = c;
+;	  }
+;	  return s;
+;	}
+;
+;	stack:
+;	+0 ret addr
+;	+2 c
+;	+4 n
+;
+
+	.code
+	.export	_memset
+_memset:
+	tsx
+	ldx	4,x		; n==0 ?
+	beq	_memset_ret
+	pshb			; save s
+	psha
+	tsx
+	ldab	7,x		; n
+	ldaa	6,x
+	cmpb	#1		; if b!=0 then a++
+	sbca	#0
+	inca
+	staa	@tmp1
+;
+	ldaa	4,x		; c
+;
+	ldx	0,x		; saved s
+;
+_memset_loop:
+	staa	0,x
+	inx
+	decb
+	bne	_memset_loop
+	dec	@tmp1
+	bne	_memset_loop
+;
+	pula
+	pulb
+;
+_memset_ret:
+	rts
+;
+
+	
+	
+	
+	
+
+
