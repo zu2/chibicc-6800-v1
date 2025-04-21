6062d21d7d649aa89c9895c4f59384abecfbc7b0
zu2 <3023910+zu2@users.noreply.github.com>
2025-04-21 15:36:10 +0900

    add test

---
+++ ztest/0350-floatassign.c
@@ -0,0 +1,22 @@
+#include "common.h"
+
+int
+main(int argc, char **argv)
+{
+	int  a[10];
+	int  b[10];
+	int  c;
+	float d = 1.0;
+
+	a[0] = b[0] = 999;
+
+	a[d-1] = b[d-1] = 555;
+
+	if (a[0]!=555)
+	  return 1;
+
+	if (b[0]!=555)
+	  return 2;
+
+	return 0;
+}
