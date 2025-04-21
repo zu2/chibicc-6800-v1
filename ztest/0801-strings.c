39dd8416cf398b04dd2b8a93fc12b2999f591d6a
zu2 <3023910+zu2@users.noreply.github.com>
2025-04-22 02:58:32 +0900

    add test.

--- ztest/0801-strings.c
+++ ztest/0801-strings.c
@@ -1,8 +1,26 @@
 #include "common.h"
 
+extern	int strcmp(char *s1, char *s2);
+
 int main(int argc, char **argv)
 {
 	putstr("Hello, World\n");
+
+	if (strcmp("Hello,","World")==0)
+	  return 1;
+
+	if (strcmp("Hello,","World")>0)
+	  return 2;
+
+	if (strcmp("Hello,","Hello,"))
+	  return 3;
+
+	if (strcmp("World","Hello,")==0)
+	  return 4;
+
+	if (strcmp("World","Hello,")<0)
+	  return 5;
+
 
 	return 0;
 }
