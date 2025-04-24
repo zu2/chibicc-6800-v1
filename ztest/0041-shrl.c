dd68e131ab3db6e0fe5bac926d1807c621f7bc7e
zu2 <3023910+zu2@users.noreply.github.com>
2025-04-24 22:59:21 +0900

    add test for https://github.com/zu2/chibicc-6800-v1/issues/17

--- ztest/0041-shrl.c
+++ ztest/0041-shrl.c
@@ -1,16 +1,109 @@
-extern void print(int i);
+#include "common.h"
 
 int main(int argc, char **argv)
 {
-	int i=1;
+	signed   char sc=1;
+	unsigned char uc=1;
+	signed   int  si=1;
+	unsigned int  ui=1;
+	signed   long sl=1;
+	unsigned long ul=1;
 
-	i <<= 8;
-	if (i!=256)
-		return 1;
+	sc <<= 6;
+	if (sc!=64)
+	  return 1;
 
-	i >>= 8;
-	if (i!=1)
-		return 2;
+	sc >>= 6;
+	if (sc!=1)
+	  return 2;
+
+	sc <<= 0;
+	if (sc!=1)
+	  return 3;
+
+	sc >>= 0;
+	if (sc!=1)
+	  return 4;
+
+	uc <<= 7;
+	if (uc!=128)
+	  return 11;
+
+	uc >>= 7;
+	if (uc!=1)
+	  return 12;
+
+	uc <<= 0;
+	if (uc!=1)
+	  return 13;
+
+	sc >>= 0;
+	if (sc!=1)
+	  return 14;
+
+	si <<= 14;
+	if (si!=16384)
+	  return 21;
+
+	si >>= 14;
+	if (si!=1)
+	  return 22;
+
+	si <<= 0;
+	if (si!=1)
+	  return 23;
+
+	si >>= 0;
+	if (si!=1)
+	  return 24;
+
+	ui <<= 14;
+	if (ui!=16384)
+	  return 31;
+
+	ui >>= 14;
+	if (ui!=1)
+	  return 32;
+
+	ui <<= 0;
+	if (ui!=1)
+	  return 33;
+
+	ui >>= 0;
+	if (ui!=1)
+	  return 34;
+
+	sl <<= 14;
+	if (sl!=16384)
+	  return 41;
+
+	sl >>= 14;
+	if (sl!=1)
+	  return 42;
+
+	sl <<= 0;
+	if (sl!=1)
+	  return 43;
+
+	sl >>= 0;
+	if (sl!=1)
+	  return 44;
+
+	ul <<= 15;
+	if (ul!=32768)
+	  return 51;
+
+	ul >>= 15;
+	if (ul!=1)
+	  return 52;
+
+	ul <<= 0;
+	if (ul!=1)
+	  return 53;
+
+	ul >>= 0;
+	if (ul!=1)
+	  return 54;
 
 	return 0;
 }
