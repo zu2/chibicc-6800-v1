200b9b321ac5b15ed1f283aad5b221b06e4c676f
zu2 <3023910+zu2@users.noreply.github.com>
2025-04-25 16:50:46 +0900

    add isnan

--- libc/__floatlib.s
+++ libc/__floatlib.s
@@ -15,9 +15,41 @@
 	.code
 ;
 ;	@long = fabs(@long)
-;	parameter passed by @long
+;		parameter passed by @long
 ;
 _fabs:
 	asl	@long		; sign bit = 0
 	lsr	@long
+	rts
+
+;
+;	AccAB = isinf(@long)
+;		parameter passed by @long
+;
+	.export	_isinf
+_isinf:
+	jsr	__f32isNaNorInf
+	bne	__isinf_not_inf
+	ldaa	@long
+	bpl	____isinf_pInf
+	ldab	#$FF		; if -Inf, return -1
+	tba
+	rts
+__isinf_not_inf:
+__isnan_not_nan:
+	clrb
+	clra
+	rts
+
+;
+;	AccAB = isnan(@long)
+;		parameter passed by @long
+;
+	.export	_isnan
+_isnan:
+	jsr	__f32isNaNorInf
+	bcc	__isnan_not_nan
+____isinf_pInf:			; if +Inf, return 1
+	clra
+	ldab	#1
 	rts
