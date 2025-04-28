2cff45931c4f5f81f844b3e8806b59421cf64de4
zu2 <3023910+zu2@users.noreply.github.com>
2025-04-28 15:52:24 +0900

    add test pattern

--- ztest/0201-longaddsub.c
+++ ztest/0201-longaddsub.c
@@ -1,3 +1,5 @@
+long	al,bl,cl,dl;
+
 int main(int argc, char **argv)
 {
 	long a,b,c,d;
@@ -10,6 +12,32 @@ int main(int argc, char **argv)
 	c = 3;
 	d = 4;
 
-	return (a-b)-(c-d);
-}
+	if ( (a-b)-(c-d) )
+	  return 2;
 
+	a = 200001;
+	b = 200000;
+	c = 300001;
+	d = 300000;
+
+	if ( (a-b)-(c-d) )
+	  return 3;
+
+	al = 1;
+	bl = 2;
+	cl = 3;
+	dl = 4;
+
+	if ( (al-bl)-(cl-dl) )
+	  return 12;
+
+	al = 200001;
+	bl = 200000;
+	cl = 300001;
+	dl = 300000;
+
+	if ( (a-b)-(c-d) )
+	  return 13;
+
+	return 0;
+}
