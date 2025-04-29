af784ce335dbc19b2411eb003d59ae545ae25287
zu2 <3023910+zu2@users.noreply.github.com>
2025-04-29 20:59:44 +0900

    add test for https://github.com/zu2/chibicc-6800-v1/issues/21

--- ztest/0906-logandor.c
+++ ztest/0906-logandor.c
@@ -1,5 +1,22 @@
 #include "common.h"
 
+int func1(void)
+{
+	if (0 || 1)
+		return 1;
+
+	return 0;
+}
+
+int func2(void)
+{
+	if (1 && 0)
+		return 1;
+
+	return 0;
+}
+
+
 int main(void) {
 	int a = 1, b = 2;
 	int r;
@@ -20,6 +37,12 @@ int main(void) {
 	r = !(a==b) && (a==b);
 	if (r!=0)
 	  return 12;
+
+	if (!func1())
+	  return 21;
+
+	if (func2())
+	  return 22;
 
 	return 0;
 }
