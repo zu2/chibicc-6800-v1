e593b9128cc96400c8c7caf890e077c14b1d2e39
zu2 <3023910+zu2@users.noreply.github.com>
2025-04-30 10:24:16 +0900

    add test

--- ztest/0911-bad-optimize.c
+++ ztest/0911-bad-optimize.c
@@ -16,20 +16,38 @@ int main(int argc, char **argv)
 	uc = ~0xff;
 	if (uc)		return 1;
 
+	uc = -0x100;
+	if (uc)		return 2;
+
 	sc = ~0xff;
 	if (sc)		return 41;
+
+	sc = -0x100;
+	if (sc)		return 42;
 
 	ui = ~0xffff;
 	if (ui)		return 81;
 
+	ui = -0x10000;
+	if (ui)		return 82;
+
 	si = ~0xffff;
 	if (si)		return 121;
+
+	si = -0x10000;
+	if (si)		return 122;
 
 	ul = ~0xffffffffL;
 	if (ul)		return 161;
 
+	ul = -0x100000000L;
+	if (ul)		return 162;
+
 	sl = ~0xffffffffL;
 	if (sl)		return 201;
+
+	sl = -0x100000000L;
+	if (sl)		return 202;
 
 	return 0;
 }
