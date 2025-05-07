13760a8a25488688ec51bbebbff6470d0178aa42
zu2 <3023910+zu2@users.noreply.github.com>
2025-05-07 13:41:21 +0900

    add header file

--- include/stddef.h
+++ include/stddef.h
@@ -1,13 +1,19 @@
-#ifndef __STDDEF_H
-#define __STDDEF_H
+// stddef.h for MC6800 C compiler
+// Defines size_t, ptrdiff_t, NULL, and offsetof
 
-#define NULL ((void *)0)
+#ifndef _STDDEF_H_
+#define _STDDEF_H_
 
-typedef unsigned long size_t;
-typedef long ptrdiff_t;
-typedef unsigned int wchar_t;
-typedef long max_align_t;
+// size_t: unsigned integer type for sizes (16bit)
+typedef unsigned int size_t;
 
+// ptrdiff_t: signed integer type for pointer differences (16bit)
+typedef int ptrdiff_t;
+
+// NULL pointer constant
+#define NULL ((void*)0)
+
+// offsetof macro: get offset of member in struct
 #define offsetof(type, member) ((size_t)&(((type *)0)->member))
 
-#endif
+#endif // _STDDEF_H_
