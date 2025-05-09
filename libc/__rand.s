21aedde59febb67bf306d6f4736ea320fabb45ec
zu2 <3023910+zu2@users.noreply.github.com>
2025-05-09 21:38:40 +0900

    Changed random number generator to the fast 24-bit PRNG algorithm. https://wimcouwenberg.wordpress.com/2020/11/15/a-fast-24-bit-prng-algorithm-for-the-6502-processor/ add rand().

--- libc/__rand.s
+++ libc/__rand.s
@@ -9,59 +9,29 @@
 __seed:	.byte	$de
 	.byte	$ad
 	.byte	$be
-__work:	.byte	0
 	.code
 	.export	_rand
 ;
-_rand:
-	ldab	__seed+2	; x ^= (x << 13) & 0xFFFFFF ; x<<13 = x<<(8+5)
-	ldaa	__seed+1
+_prng24:
+	ldab	__seed
 	aslb
-	rola
-	aslb
-	rola
-	aslb
-	rola
-	aslb
-	rola
-	aslb
-	rola
-	eorb	__seed+1
-	eora	__seed
-	stab	__seed+1
-	staa	__seed
-;
-;	ldaa	__seed		; x ^= (x >> 17) ; x>>17 = x>>(16+1)
-	lsra
-	eora	__seed+2
-	staa	__seed+2	
-;				; now AccB == seed+1, AccA == seed+2
-	staa	__work
-	ldaa	__seed
-	asl	__work		; x<<5
-	rolb
-	rola
-	asl	__work
-	rolb
-	rola
-	asl	__work
-	rolb
-	rola
-	asl	__work
-	rolb
-	rola
-	asl	__work
-	rolb
-	rola
 	eorb	__seed+1
 	stab	__seed+1
-	eora	__seed
-	staa	__seed
-	ldab	__work
+	rolb
 	eorb	__seed+2
 	stab	__seed+2
-;
-	ldaa	__seed		; now, AccA:B = __seed:__seed+1
+	eorb	__seed
+	stab	__seed
+	ldab	__seed+1
+	rorb
+	eorb	__seed+2
+	stab	__seed+2
+	eorb	__seed+1
+	stab	__seed+1
+	rts
+_rand:
+	bsr	_prng24
+	ldaa	__seed
 	lsra
 	rorb
 	rts
@@ -82,5 +52,5 @@ __seed_not_zero:
 	stab	__seed+1
 	staa	__seed
 	eorb	__seed
-	stab	__seed+1
+	stab	__seed+2
 	rts
