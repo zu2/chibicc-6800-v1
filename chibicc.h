24484828c71d766108a62f582b6189e5c6679ffe
zu2 <3023910+zu2@users.noreply.github.com>
2025-05-09 21:12:38 +0900

    optimize: if (var) pattern

--- chibicc.h
+++ chibicc.h
@@ -444,6 +444,8 @@ __attribute__((format(printf, 1, 2)))
 void println(char *fmt, ...);
 __attribute__((format(printf, 1, 2)))
 void printout(char *fmt, ...);
+bool is_local_var(Node *node);
+bool is_global_var(Node *node);
 int  test_addr_x(Node *node);
 void codegen(Obj *prog, FILE *out);
 int align_to(int n, int align);
