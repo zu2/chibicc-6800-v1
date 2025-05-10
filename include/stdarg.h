9bb581f3a91c6356042955820995d9ca1c33035f
zu2 <3023910+zu2@users.noreply.github.com>
2025-05-11 06:35:24 +0900

    add va_copy

--- include/stdarg.h
+++ include/stdarg.h
@@ -1,21 +1,11 @@
 #ifndef STDARG_H
 #define STDARG_H
 
-// va_list is a pointer type used to traverse variable arguments
 typedef char *va_list;
-//static	char *__builtin_va_start_addr();
 
-// Initialize 'ap' to point to the first variable argument after 'last'.
-// __builtin_va_start_addr(last) is a compiler builtin that returns the address
-// immediately after the last fixed argument.
 #define va_start(ap, last) (ap = __builtin_va_start_addr(last))
-
-// Retrieve the next argument of type 'type' from the argument list.
-// Advances 'ap' to the next argument.
 #define va_arg(ap, type) (*(type *)((ap += sizeof(type)) - sizeof(type)))
-
-// Clean up the va_list. This is usually a no-op, but we reset ap to NULL for safety.
-#define va_end(ap) ((void)(ap = (va_list)0))
-
+#define	va_end(ap) ((void)(ap = (va_list)0))
+#define va_copy(dest, src) ((dest) = (src))
 
 #endif // STDARG_H
