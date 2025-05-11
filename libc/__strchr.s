0e1748a4b1ae475d1e574902e0788e431e5c60b5
zu2 <3023910+zu2@users.noreply.github.com>
2025-05-12 05:28:06 +0900

    bug fix add strrchr

--- libc/__strchr.s
+++ libc/__strchr.s
@@ -29,19 +29,18 @@ _strchr:
 _strchr_loop:
 	inx
 	ldaa	0,x
-	beq	_strchr_end
 	cba
-	bne	_strchr_loop
+	beq	_strchr_found
+	tsta
+	bne	_strchr_end
+_strchr_end:
+	clrb
+;	clra			; here, AccA == 0
+	rts
 _strchr_found:
 	stx	@tmp1
 	ldab	@tmp1+1
 	ldaa	@tmp1
-	rts
-_strchr_end:
-	tstb
-	beq	_strchr_found	; found '\0'
-	clrb
-;	clra
 	rts
 ;
 
---
+++ libc/__strrchr.s
@@ -0,0 +1,56 @@
+;
+;	char *strrchr(const char *s,int c)
+;	{
+;	  char *p = NULL;
+;
+;	  while (*s) {
+;	    if (*s==c)
+;             p = s;
+;	      s++;
+;	  }
+;	  return p;
+;	}
+;
+;	AccAB: s
+;
+;	stack:
+;	+0 ret addr
+;	+2 c
+;
+
+	.code
+	.export	_strchr
+_strchr:
+	stab	@tmp1+1		; save s
+	staa	@tmp1
+;
+	clr	@tmp2+1		; p = NULL
+	clr	@tmp2
+;
+	tsx
+	ldab	3,x		; get c
+;
+	ldx	@tmp1
+	dex
+;
+_strchr_loop:
+	inx
+	ldaa	0,x
+	cba
+	bne	_strchr_ne
+	stx	@tmp2
+_strchr_ne:
+	tsta
+	bne	_strchr_loop
+;
+	ldab	@tmp2+1
+	ldaa	@tmp2
+	rts
+;
+
+	
+	
+	
+	
+
+
