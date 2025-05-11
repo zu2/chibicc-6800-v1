fd8a18f194a3f405ec7ef0c255de1f5e0932db0f
zu2 <3023910+zu2@users.noreply.github.com>
2025-05-12 04:53:05 +0900

    fix test: delete double, long long. int=16bit, long=32bit

--- test/sizeof.c
+++ test/sizeof.c
@@ -5,19 +5,19 @@ int main() {
   ASSERT(2, sizeof(short));
   ASSERT(2, sizeof(short int));
   ASSERT(2, sizeof(int short));
-  ASSERT(4, sizeof(int));
-  ASSERT(8, sizeof(long));
-  ASSERT(8, sizeof(long int));
-  ASSERT(8, sizeof(long int));
-  ASSERT(8, sizeof(char *));
-  ASSERT(8, sizeof(int *));
-  ASSERT(8, sizeof(long *));
-  ASSERT(8, sizeof(int **));
-  ASSERT(8, sizeof(int(*)[4]));
-  ASSERT(32, sizeof(int*[4]));
-  ASSERT(16, sizeof(int[4]));
-  ASSERT(48, sizeof(int[3][4]));
-  ASSERT(8, sizeof(struct {int a; int b;}));
+  ASSERT(3, sizeof(int));
+  ASSERT(4, sizeof(long));
+  ASSERT(4, sizeof(long int));
+  ASSERT(4, sizeof(long int));
+  ASSERT(2, sizeof(char *));
+  ASSERT(2, sizeof(int *));
+  ASSERT(2, sizeof(long *));
+  ASSERT(2, sizeof(int **));
+  ASSERT(2, sizeof(int(*)[4]));
+  ASSERT(8, sizeof(int*[4]));
+  ASSERT(8, sizeof(int[4]));
+  ASSERT(24, sizeof(int[3][4]));
+  ASSERT(4, sizeof(struct {int a; int b;}));
 
   ASSERT(8, sizeof(-10 + (long)5));
   ASSERT(8, sizeof(-10 - (long)5));
@@ -64,28 +64,28 @@ int main() {
   ASSERT(8, sizeof(unsigned long));
   ASSERT(8, sizeof(unsigned long int));
 
-  ASSERT(8, sizeof(long long));
-  ASSERT(8, sizeof(signed long long));
-  ASSERT(8, sizeof(signed long long int));
-  ASSERT(8, sizeof(unsigned long long));
-  ASSERT(8, sizeof(unsigned long long int));
+//ASSERT(8, sizeof(long long));
+//ASSERT(8, sizeof(signed long long));
+//ASSERT(8, sizeof(signed long long int));
+//ASSERT(8, sizeof(unsigned long long));
+//ASSERT(8, sizeof(unsigned long long int));
 
   ASSERT(1, sizeof((char)1));
   ASSERT(2, sizeof((short)1));
-  ASSERT(4, sizeof((int)1));
-  ASSERT(8, sizeof((long)1));
+  ASSERT(2, sizeof((int)1));
+  ASSERT(4, sizeof((long)1));
 
-  ASSERT(4, sizeof((char)1 + (char)1));
-  ASSERT(4, sizeof((short)1 + (short)1));
-  ASSERT(4, sizeof(1?2:3));
-  ASSERT(4, sizeof(1?(short)2:(char)3));
-  ASSERT(8, sizeof(1?(long)2:(char)3));
+  ASSERT(2, sizeof((char)1 + (char)1));
+  ASSERT(2, sizeof((short)1 + (short)1));
+  ASSERT(2, sizeof(1?2:3));
+  ASSERT(2, sizeof(1?(short)2:(char)3));
+  ASSERT(4, sizeof(1?(long)2:(char)3));
 
   ASSERT(1, sizeof(char) << 31 >> 31);
   ASSERT(1, sizeof(char) << 63 >> 63);
 
   ASSERT(4, sizeof(float));
-  ASSERT(8, sizeof(double));
+//ASSERT(8, sizeof(double));
 
   ASSERT(4, sizeof(1f+2));
   ASSERT(8, sizeof(1.0+2));
@@ -94,9 +94,9 @@ int main() {
   ASSERT(4, sizeof(1f*2));
   ASSERT(8, sizeof(1.0*2));
   ASSERT(4, sizeof(1f/2));
-  ASSERT(8, sizeof(1.0/2));
+//ASSERT(8, sizeof(1.0/2));
 
-  ASSERT(16, sizeof(long double));
+//ASSERT(16, sizeof(long double));
 
   ASSERT(1, sizeof(main));
 
