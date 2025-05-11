818f28fa2c410965ee4f4542f0ccbfc36be0d6b8
zu2 <3023910+zu2@users.noreply.github.com>
2025-05-12 01:22:04 +0900

    fix test

--- test/asm.c
+++ test/asm.c
@@ -1,17 +1,15 @@
 #include "test.h"
 
 char *asm_fn1(void) {
-  asm("mov $50, %rax\n\t"
-      "mov %rbp, %rsp\n\t"
-      "pop %rbp\n\t"
-      "ret");
+  asm("ldab #50\n\t"
+      "ldaa #0\n\t"
+      "rts");
 }
 
 char *asm_fn2(void) {
-  asm inline volatile("mov $55, %rax\n\t"
-                      "mov %rbp, %rsp\n\t"
-                      "pop %rbp\n\t"
-                      "ret");
+  asm inline volatile("ldab #55\n\t"
+                      "ldaa #0\n\t"
+                      "rts");
 }
 
 int main() {
