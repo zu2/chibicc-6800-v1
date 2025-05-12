06d4ab3d92b60f456df32775ede9c25038a93f5c
zu2 <3023910+zu2@users.noreply.github.com>
2025-05-12 22:51:54 +0900

    more -Os staff

---
+++ libc/__popx.s
@@ -0,0 +1,24 @@
+;
+;	popx()
+;
+;	+0: return address
+;	+2: pop data
+;
+	.export __popx
+	.code
+;
+__popx:
+	tsx
+	ldx  2,x
+	stab @tmp1+1
+	staa @tmp1
+	pula
+	pulb
+	ins
+	ins
+	pshb
+	psha
+	ldab @tmp1+1
+	ldaa @tmp1
+	rts
+;
