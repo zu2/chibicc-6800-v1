bbf03adc8477d967a90c50ae99234e4d3f0c010f
zu2 <3023910+zu2@users.noreply.github.com>
2025-05-15 08:50:57 +0900

    Improved accuracy

--- libm/atanf.c
+++ libm/atanf.c
@@ -1,19 +1,21 @@
+#include <stdio.h>
 #include <math.h>
 
-float atanf(float x)
-{
-  const float a1 = 0.99997726f, a3 = -0.33262347f, a5 = 0.19354346f;
-  const float a7 = -0.11643287f, a9 = 0.05265332f, a11 = -0.01172120f;
+float atanf(float x) {
+    float r = 0.0f;
+    float x2 = x*x;
 
-  if (fabsf(x) <= 1.0f) {
-    float x2 = x * x;
-    return x * (a1 + x2 * (a3 + x2 * (a5 + x2 * (a7 + x2 * (a9 + x2 * a11)))));
-  } else {
-    float inv = 1.0f / fabsf(x);
-    float x2 = inv * inv;
-    float core =
-        inv * (a1 + x2 * (a3 + x2 * (a5 + x2 * (a7 + x2 * (a9 + x2 * a11)))));
-    float y = 1.57079637f - core; // π/2
-    return (x < 0) ? -y : y;
-  }
+    r = (121.0f * x2) / (23.0f + r);  // 11
+    r = (100.0f * x2) / (21.0f + r);  // 10
+    r = ( 81.0f * x2) / (19.0f + r);  //  9
+    r = ( 64.0f * x2) / (17.0f + r);  //  8
+    r = ( 49.0f * x2) / (15.0f + r);  //  7
+    r = ( 36.0f * x2) / (13.0f + r);  //  6
+    r = ( 25.0f * x2) / (11.0f + r);  //  5
+    r = ( 16.0f * x2) / ( 9.0f + r);  //  4
+    r = (  9.0f * x2) / ( 7.0f + r);  //  3
+    r = (  4.0f * x2) / ( 5.0f + r);  //  2
+    r = (         x2) / ( 3.0f + r);  //  1
+
+    return (x / (1.0f + r)); // 0
 }
