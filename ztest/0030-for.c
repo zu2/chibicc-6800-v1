d9e2ce92815206b11205a2719e3459181381f977
zu2 <3023910+zu2@users.noreply.github.com>
2025-05-21 08:20:46 +0900

    add test pattern for optimizer check

--- ztest/0030-for.c
+++ ztest/0030-for.c
@@ -4,6 +4,16 @@ int main(int argc, char **argv)
 {
 	int	i;
 
+  for (;;) {                // optimizer check
+    i = 0;
+    break;
+  }
+  for (;1;) {                // optimizer check
+    i = 0;
+    break;
+  }
+  for (;0;) {                // optimizer check
+  }
 	for (i=0; i<1000; i++){
 		if(i==50)
 			break;
--- ztest/0031-while.c
+++ ztest/0031-while.c
@@ -2,7 +2,14 @@ int main(int argc, char **argv)
 {
 	int	i;
 
-	i = 0;
+  while (1) {         // optimizer check
+    i = 0;
+    break;
+  }
+  while (0) {         // optimizer check
+    i = 0;
+  }
+
 	while (i<1000){
 		if(i==50)
 			break;
