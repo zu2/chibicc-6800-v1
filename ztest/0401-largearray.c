8ab197cf08df466761a22176ea36942984f717f1
zu2 <3023910+zu2@users.noreply.github.com>
2025-05-21 22:44:22 +0900

    change test pattern

--- ztest/0401-largearray.c
+++ ztest/0401-largearray.c
@@ -1,39 +1,55 @@
 #include "common.h"
 
-int	a[1024];
+int a[1024];
 
-int
-main(argc, char **argv)
+int main(argc, char **argv)
 {
-	int b[1024];
-	long asum=0;
-	long bsum=0;
+  int b[1024];
+  int *c = &b[1000];
+  long asum = 0;
+  long bsum = 0;
 
-	for (int i=0; i<1000; i=i+1) {
-		a[i] = i;
-	}
-//	for (int i=0; i<1000; i=i+1) {
-//		print(a[i]);
-//	}
-	for (int i=0; i<1000; i=i+1) {
-		asum += a[i];
-	}
-	puthexl(asum);
-	if (asum!=499500)
-		return 1;
+  for (int i = 0; i < 1000; i = i + 1) {
+    a[i] = i;
+  }
+  for (int i = 0; i < 1000; i = i + 1) {
+    asum += a[i];
+  }
+  //	puthexl(asum);
+  if (asum != 499500) {
+    return 1;
+  }
 
-	for (int i=0; i<1000; i=i+1) {
-		b[i] = i;
-	}
-//	for (int i=0; i<1000; i=i+1) {
-//		print(b[i]);
-//	}
-	for (int i=0; i<1000; i=i+1) {
-		bsum += b[i];
-	}
-	puthexl(bsum);
-	if (bsum!=499500)
-		return 1;
+  for (int i = 0; i < 1000; i = i + 1) {
+    b[i] = i;
+  }
+  for (int i = 0; i < 1000; i = i + 1) {
+    bsum += b[i];
+  }
+  //	puthexl(bsum);
+  if (bsum != 499500) {
+    return 11;
+  }
 
-	return 0;
+  if (a[0] != 0) {
+    return 2;
+  }
+
+  if (a[999] != 999) {
+    return 3;
+  }
+
+  if (b[0] != 0) {
+    return 12;
+  }
+
+  if (b[999] != 999) {
+    return 13;
+  }
+
+  if (c[-1] != 999) {
+    return 14;
+  }
+
+  return 0;
 }
