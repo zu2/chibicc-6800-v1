391f1501fa3d8576c9efe6aa5c6390447a4c08fe
zu2 <3023910+zu2@users.noreply.github.com>
2025-05-21 22:45:41 +0900

    fix test

--- ztest/9090-ptrptr.c
+++ ztest/9090-ptrptr.c
@@ -1,31 +1,37 @@
-typedef	struct {
-	unsigned char *left;
-	unsigned char *right;
+typedef struct {
+  unsigned char *left;
+  unsigned char *right;
 } node;
 
-equalp(node *p,node *q)
+equalp(node *p, node *q)
 {
-	unsigned char dummy;
+  unsigned char dummy;
 
-	dummy = *(p->left) + *(q->left);
+  dummy = *(p->left) + *(q->left);
 
-	if (*(p->left) != *(q->left))
-		return	0;
-	if (*(p->right) != *(q->right))
-		return	0;
-	return 1;
+  if (*(p->left) == *(q->left)) {
+    return 1;
+  }
+  if (*(p->right) == *(q->right)) {
+    return 2;
+  }
+  return 0;
 }
 
 int main(int argc, char **argv)
 {
-	node	p,q;
-	unsigned char a[5];
+  node p, q;
+  unsigned char a[5];
 
-	a[0] = 0; a[1] = 1; a[2] = 2; a[3] = 3; a[4] = 4;
-	p.left  = &a[1];
-	p.right = &a[2];
-	q.left  = &a[3];
-	q.right = &a[4];
+  a[0] = 0;
+  a[1] = 1;
+  a[2] = 2;
+  a[3] = 3;
+  a[4] = 4;
+  p.left = &a[1];
+  p.right = &a[2];
+  q.left = &a[3];
+  q.right = &a[4];
 
-	return equalp(&p,&q);
+  return equalp(&p, &q);
 }
