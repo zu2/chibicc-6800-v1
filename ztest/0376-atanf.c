a91881f7eae0a009021946fae79eab2712e664f0
zu2 <3023910+zu2@users.noreply.github.com>
2025-05-27 11:50:33 +0900

    fix test

--- ztest/0376-atanf.c
+++ ztest/0376-atanf.c
@@ -4,6 +4,52 @@
 
 #include "my_printf.c"
 
+static float atanf_taylor(float x)
+{
+  float x2 = x * x;
+  return x - (x2 * x) / 3.0f + (x2 * x2 * x) / 5.0f -
+         (x2 * x2 * x2 * x) / 7.0f + (x2 * x2 * x2 * x2 * x) / 9.0f;
+}
+
+static float atanf_frac(float x)
+{
+  float r = 0.0f;
+  float x2 = x * x;
+
+  r = (121.0f * x2) / (23.0f + r); // 11
+  r = (100.0f * x2) / (21.0f + r); // 10
+  r = (81.0f * x2) / (19.0f + r);  //  9
+  r = (64.0f * x2) / (17.0f + r);  //  8
+  r = (49.0f * x2) / (15.0f + r);  //  7
+  r = (36.0f * x2) / (13.0f + r);  //  6
+  r = (25.0f * x2) / (11.0f + r);  //  5
+  r = (16.0f * x2) / (9.0f + r);   //  4
+  r = (9.0f * x2) / (7.0f + r);    //  3
+  r = (4.0f * x2) / (5.0f + r);    //  2
+  r = (x2) / (3.0f + r);           //  1
+
+  return (x / (1.0f + r)); // 0
+}
+
+float myatanf(float x)
+{
+  if (isnan(x)) {
+    return NAN;
+  }
+  if (isinf(x)) {
+    return copysignf(M_PI_2,x);
+  }
+  if (x < 0) {
+    return -myatanf(fabsf(x));
+  }else if (fabsf(x) < 0.5f) {
+    return atanf_taylor(x);
+  } else if (fabsf(x) <= 1.0f) {
+    return atanf_frac(fabsf(x));
+  } else {
+    float inv_x = 1.0f / fabsf(x);
+    return M_PI_2 - myatanf(inv_x);
+  }
+}
 struct TestCase {
   float input;
   float expected;
@@ -54,6 +100,7 @@ bool test_atanf()
   print_header();
   for (int i = 0; i < sizeof(test_cases) / sizeof(test_cases[0]); i++) {
     float result = atanf(test_cases[i].input);
+    //float result = myatanf(test_cases[i].input);
     float diff = fabsf(result - test_cases[i].expected);
     bool is_nan = isnan(test_cases[i].expected);
     bool is_pass;
