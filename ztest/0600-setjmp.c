9f60d89478e759a2a1a47ca934c03ef791e73ca3
zu2 <3023910+zu2@users.noreply.github.com>
2025-05-30 10:15:23 +0900

    There is an issue with the setjmp/longjmp test: test 0600-setjmp fails. setjmp is only available for use as specified in section 7.13.1.1 of [ISO/IEC 9899:1999], but the test attempted to assign its return value, which prevented correct results.

--- ztest/0600-setjmp.c
+++ ztest/0600-setjmp.c
@@ -1,4 +1,4 @@
-#include "common.h"
+#include <stdlib.h>
 #include <setjmp.h>
 
 jmp_buf env;
@@ -7,19 +7,21 @@ void subroutine(int n) {
     if (n == 0) {
         longjmp(env, 123);
     }
-    putstr("longjmp error\n");
+    exit(1);
 }
 
 int main(int argc, char **argv) {
     int ret;
 
-    ret = setjmp(env);
-
-    if (ret == 0) {
-        subroutine(0);
-        return 1;
-    } else {
-        return (ret == 123) ? 0 : 2;
+    switch(setjmp(env)) {
+    case 0:
+      subroutine(0);
+      return 1;
+    case 123:
+      return 0;
+    default:
+      return 2;
     }
+
     return 99;
 }
