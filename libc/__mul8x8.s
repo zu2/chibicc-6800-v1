fcfd9818f176c3f653c29b38b85272958dbbd74d
zu2 <3023910+zu2@users.noreply.github.com>
2025-06-12 10:00:55 +0900

    speed up 15%

--- libc/__mul8x8.s
+++ libc/__mul8x8.s
@@ -5,15 +5,12 @@
 	.code
 __mul8x8:
         stab    @tmp1+1
-        staa    @tmp1
 ;
-        clra
         clrb
         ldx     #8
 __mul8x8_10:
         aslb
         rola
-        rol     @tmp1
         bcc     __mul8x8_20
         addb    @tmp1+1
         adca    #0
