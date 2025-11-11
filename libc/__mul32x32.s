40c52de9ef7ca260cf7b4cac417f13ea4cebb8f9
zu2 <3023910+zu2@users.noreply.github.com>
2025-11-11 16:41:26 +0900

    speed up: 25%
    speed up 1.5%

--- libc/__mul32x32.s
+++ libc/__mul32x32.s
@@ -5,86 +5,77 @@
 ;
 	.export __mul32tos
 	.code
-shift8:	ldab	@long+1
-	stab	@long
-	ldab	@long+2
-	stab	@long+1
-	ldab	@long+3
-	stab	@long+2
-	clr	long+3
-	ldab	@tmp3
-	subb	#8
-	stab	@tmp3
-	rts
 ;
-;  2,x-5,x:    Multiplicand
-;  tmp2,AccAB: multiplier
-;  long:       result
+;  __mul32tos: @long = @long * TOS
+;
+;  0-1,x:      return address
+;  2-5,x:      Multiplicand (TOS) & result
+;  tmp2:AccAB  working space
+;  long:       Multiplier
+;  tmp1:       loop counter
+;  tmp4:       loop end (IX)
 ;
 __mul32tos:
-	ldab #32
-	stab @tmp3
-	tsx
-	; 0-1,x return address
-	; 2-5,x TOS
-	clra
-	clrb
-	stab	@tmp2+1
-	staa	@tmp2
+        ldx #0
+        stx @tmp3
+        stx @tmp2
 ;
-	tst	@long
-	bne	first
+        tsx
+        inx
+        stx @tmp4
+        inx
+        inx
+        inx
+        inx
 ;
-	tst	@long+1
-	bne	skip8	; @long[0] == 0, skip 8bit
+        clrb
+        pshb
+        clra
 ;
-	tst	@long+2
-	bne	skip16	; @long[0..1] ==0, skip 16bit
+loop8:
+        ldab #$08
+        stab @tmp1
+loop:
+        ldab 0,x
+        andb #$01
+        pulb
+        beq  skip
 ;
-	tst	@long+3
-	bne	skip24	; @long[0..2] ==0, skip 24bit
-;	; all 0! return 0
-	bra	ret
+        addb @long+3
+        adca @long+2
+        psha
+        ldaa @tmp2+1
+        adca @long+1
+        staa @tmp2+1
+        ldaa @tmp2
+        adca @long
+        staa @tmp2
+        pula
 ;
-skip24:	bsr	shift8
-skip16:	bsr	shift8
-skip8:	bsr	shift8
-	clrb
-	bra	first
+skip:   lsr @tmp2
+        ror @tmp2+1
+        rora
+        rorb
+        ror 0,x
 ;
-;	Rotate through the number
-nextbit:
-	aslb
-	rola
-	rol	@tmp2+1
-	rol	@tmp2
-first:	rol	@long+3
-	rol	@long+2
-	rol	@long+1
-	rol	@long
-	bcc noadd
-	addb	5,x
-	adca	4,x
-	psha
-	ldaa	3,x
-	adca	@tmp2+1
-	staa	@tmp2+1
-	ldaa	2,x
-	adca	@tmp2
-	staa	@tmp2
-	pula
-noadd:
-	dec @tmp3
-	bne nextbit
-	; The result of 32bit*32bit is 64bit. It is stored in 2-5,x and @long.
-	stab	@long+3
-	staa	@long+2
-	ldab	@tmp2+1
-	stab	@long+1
-	ldab	@tmp2
-	stab	@long
+next:   pshb
+        dec @tmp1
+        bne loop
+next8:
+        dex
+        cpx @tmp4
+        bne loop8
+        ins             ; pulb
 ret:
-	ldx	0,x
+        tsx
+        ldx 2,x
+        stx @long
+        tsx
+        ldx 4,x
+        stx @long+2
+;
+        tsx
+	ldx 0,x
 	ins
 	ins
 	ins
