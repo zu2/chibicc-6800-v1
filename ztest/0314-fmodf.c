7bde143895bed866a6ab86f12c25b07694cbbc9b
zu2 <3023910+zu2@users.noreply.github.com>
2025-11-13 20:03:21 +0900

    Revise handling of hard-to-compute values; update tests accordingly.

--- ztest/0314-fmodf.c
+++ ztest/0314-fmodf.c
@@ -42,6 +42,7 @@ int main(int argc, char **argv)
 	float	f,g,h,e,pzero,mzero,pinf,minf;
 
 #if 0
+#if 1
 	pzero = to_float(pZERO);
 	mzero = to_float(mZERO);
 	pinf  = to_float(pINFINITY);
@@ -70,8 +71,8 @@ int main(int argc, char **argv)
 	if (fmodf(f,g)!=f)		// f>g, then f
 		return 10;
 
-	puthexf(fmodf(g,f));putstr(" fmod(2.0f,1.0f)\n");
-	puthexf(0.0f);putstr(" 0.0f\n");
+//puthexf(fmodf(g,f));putstr(" fmod(2.0f,1.0f)\n");
+//puthexf(0.0f);putstr(" 0.0f\n");
 	if (fmodf(g,f)!=0.0f)
 		return 11;
 
@@ -101,6 +102,7 @@ int main(int argc, char **argv)
 	h = 0.003f;
 	e = 9001.5f;
 	puthexf((float)fmodf(f,g));putstr(" fmod(1.0f,0.3f)\n");
+	puthexf((float)my_fmodf(f,g));putstr(" my_fmod(1.0f,0.3f)\n");
 #if 0
 	puthexf(0.1f);putstr(" 0.1f\n");
 //	if (fmodf(f,g)!=0.1f)
@@ -110,15 +112,16 @@ int main(int argc, char **argv)
 	f = 2.0f;
        	g = 0.3f;
 	puthexf((float)fmodf(f,g));putstr(" fmod(2.0f,0.3f)\n");
+	puthexf((float)my_fmodf(f,g));putstr(" my_fmod(2.0f,0.3f)\n");
 #if 0
 	puthexf(0.2f);putstr(" 0.2f\n");
 #endif
 
 	f = 5.3f;
-       	g = 0.1f;
+ 	g = 0.1f;
 	puthexf((float)fmodf(f,g));putstr(" fmod(5.3f,0.1f)\n");
 	puthexf(my_fmodf(f,g));putstr(" my_fmodf(5.3f,0.1f)\n");
-	puthexf(fabsf(fmodf(f,g)-my_fmodf(f,g))/my_fmodf(f,g));putstr(" error\n");
+	puthexf(fabsf(fmodf(f,g)-my_fmodf(f,g))/my_fmodf(f,g));putstr(" ↑error,but ok\n");
 #if 0
 	puthexf(0.2f);putstr(" 0.2f\n");
 #endif
@@ -126,110 +129,19 @@ int main(int argc, char **argv)
 	f = 200.0f;
 	g = 0.3f;
 	puthexf((float)fmodf(f,g));putstr(" fmod(200.0f,0.3f)\n");
-#if 0
-	f = 1.0f;
-	g = to_float(HEX_FLT_MIN_DIV2);
-	e = f/g;
-	if (isinf(e))
-		return 101;
-	if (cmpfl(e,0x7F000000))
-		return 102;
-
-	f = 1.0f;
-	g = to_float(HEX_FLT_MIN_DIV4);
-	e = f/g;
-	if (!isinf(e))
-		return 111;
-	if (isinf(e)!=1)
-		return 112;
-
-	f = -1.0f;
-	g = to_float(HEX_FLT_MIN_DIV4);
-	e = f/g;
-	if (!isinf(e))
-		return 121;
-	if (isinf(e)!=-1)
-		return 122;
-
-	f = to_float(HEX_FLT_MIN_DIV2);
-	g = 2.0f;
-	e = f/g;
-	if (e != to_float(HEX_FLT_MIN_DIV4))
-		return 131;
-	e = e/g;
-	if (e != to_float(HEX_FLT_MIN_DIV8))
-		return 132;
-
-	f = to_float(HEX_FLT_TRUE_MIN2);
-	g = to_float(HEX_FLT_TRUE_MIN);
-	e = f/g;
-	if (e!=2.0f)
-		return 141;
-
-
-	f = to_float(HEX_FLT_MIN);
-	g = 0.0;
-	e = f/g;
-	if (!isinf(e))
-		return 151;
-
-	f = to_float(HEX_FLT_TRUE_MIN);
-	g = 0.0;
-	e = f/g;
-	if (!isinf(e))
-		return 152;
-
-	f = pzero;
-	g = pzero;
-	e = f/g;
-	if (!isnan(e))
-		return 161;
-
-	f = mzero;
-	g = mzero;
-	e = f/g;
-	if (!isnan(e))
-		return 162;
-
-	f = 1.0f;
-	g = to_float(pINFINITY);
-	e = f/g;
-
-	if (e!=0.0)
-		return 171;
-
-	if (cmpfl(e,pZERO))
-		return 172;
-
-	if (!cmpfl(e,mZERO))
-		return 173;
-
-	f = 1.0f;
-	g = to_float(mINFINITY);
-	e = f/g;
-
-	if (e!=0.0)
-		return 181;
-
-	if (!cmpfl(e,pZERO))
-		return 182;
-
-	if (cmpfl(e,mZERO))
-		return 183;
-
-	// round check. more test needed.
-	f = 2.5f;
-	g = 3.0f;
-	e = f/g;
-	if (e!=0.8333333f)
-		return 201;
-
-	f = 2.9999999f;
-	g = 3.0f;
-	e = f/g;
-	if (e != 0.99999999f)
-		return 202;
+	puthexf((float)my_fmodf(f,g));putstr(" my_fmod(200.0f,0.3f)\n");
 
 #endif
+  f = 3.40282350e+38f;
+  g = 0x1.921fb54p+2f; // PI*2.0
+//puthexf(f);putchar(' ');puthexf(g);putchar('\n');
+//puthexf((float)fmodf(f,g));putstr(" fmod(3.40282350e+38f,0x1.921fb54p+2f)\n");
+//puthexf((float)my_fmodf(f,g));putstr(" my_fmod(3.40282350e+38f,0x1.921fb54p+2f)\n");
+// fmodf(f/g) returns NaN; but OK...
+  if (fmodf(f,g)>g) {
+    putstr("fmodf(f,g)>g\n");
+    return 99;
+  }
+
 	return 0;
 }
