9cafcb679f86dff9ea8e79657564a6d2be75daa6
zu2 <3023910+zu2@users.noreply.github.com>
2026-01-06 06:40:43 +0900

    add startup

---
+++ crt0_mikbug.s
@@ -0,0 +1,78 @@
+;
+;	crt0 for MIKBUG
+;
+	.zp
+	.export bp
+	.export tmp1
+	.export tmp2
+	.export tmp3
+	.export tmp4
+	.export long
+bp:	.word	0
+tmp1:	.word	0
+tmp2:	.word	0
+tmp3:	.word	0
+tmp4:	.word	0
+long:	.word	0
+	.word	0
+;
+	.export _putchar
+	.export _getchar
+	.export _exit
+	.export _abort
+	.code
+start:
+	sts exitsp
+;	lds <RANEND
+	ldx #__bss_size
+	beq call_main
+	ldx #__bss
+;
+	ldab #<__bss
+	ldaa #>__bss
+	addb #<__bss_size
+	adca #>__bss_size
+	stab @tmp1+1
+	staa @tmp1
+	clrb
+bss_clear:
+	stab 0,x
+	inx
+	cpx @tmp1
+	bne bss_clear
+;
+call_main:
+	clra
+	clrb
+	pshb
+	psha
+	jsr _main
+	ins
+	ins
+;
+;	No atexit support yet
+;
+_exit:
+_abort:
+doexit:
+	lds exitsp
+	rts
+;
+;	minimal I/O routine
+;
+_putchar:
+        tba
+        jmp $e1d1
+_getchar:
+        jsr $e1ac
+        tab
+        clra
+        rts
+_kbhit:
+        swi
+;
+        .data
+exitsp:
+	.word 0
+	.bss
+
