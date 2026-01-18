2f4c8439f08445c17cea0a5b01cd3a19c1205ef7
zu2 <3023910+zu2@users.noreply.github.com>
2026-01-18 11:02:25 +0900

    add test pattern

--- ztest/0907-bool.c
+++ ztest/0907-bool.c
@@ -1,30 +1,56 @@
 #include "common.h"
 
-int main(void) {
-	int a = 1, b = 2;
-	_Bool r;
+int main(void)
+{
+  int a = 1, b = 2;
+  char c = 5;
+  long l = 1000000L;
+  float f = 1.0;
+  _Bool r;
 
+  r = (a == b) || !(a == b);
+  if (r != 1) {
+    return 1;
+  }
 
-	r = (a==b) || !(a==b);
-	if (r!=1)
-	  return 1;
+  r = !(a == b) || (a == b);
+  if (r != 1) {
+    return 2;
+  }
 
-	r = !(a==b) || (a==b);
-	if (r!=1)
-	  return 2;
+  r = (a == b) && !(a == b);
+  if (r != 0) {
+    return 11;
+  }
 
-	r = (a==b) && !(a==b);
-	if (r!=0)
-	  return 11;
+  r = !(a == b) && (a == b);
+  if (r != 0) {
+    return 12;
+  }
 
-	r = !(a==b) && (a==b);
-	if (r!=0)
-	  return 12;
+  if (!(r == 0)) {
+    return 13;
+  }
+  if (!(r != 1)) {
+    return 14;
+  }
 
-	if (!(r==0))
-	  return 13;
-	if (!(r!=1))
-	  return 14;
+  r = 5;
+  if (r != 1) {
+    return 20;
+  }
+  r = c;
+  if (r != 1) {
+    return 21;
+  }
+  r = l;
+  if (r != 1) {
+    return 22;
+  }
+  r = f;
+  if (r != 1) {
+    return 23;
+  }
 
-	return 0;
+  return 0;
 }
