d3fe289d4007be50def9bb7ed332511f63242da8
zu2 <3023910+zu2@users.noreply.github.com>
2026-03-11 11:08:18 +0900

    add test pattern

--- ztest/0001-intaddsub.c
+++ ztest/0001-intaddsub.c
@@ -1,4 +1,12 @@
 int main(int argc, char **argv)
 {
+  for (long ii=-32768; ii<=32767; ii++) {
+    int i = ii;
+    int j = i - 10 + 'A';
+
+    if (j != i+0x37)
+      return 99;
+  }
+
 	return (1-2)-(3-4);
 }
