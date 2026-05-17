e7b8047a2bb52c50128ac5e786c865fcf3ab77bc
zu2 <3023910+zu2@users.noreply.github.com>
2026-05-18 03:56:04 +0900

    bug fix

--- libc/__strcmp.s
+++ libc/__strcmp.s
@@ -24,34 +24,40 @@ _strcmp:
 ;
 	tsx
 	ldx	2,x		; get s2
-	stx	@tmp3
+	bra	start
 ;
-_strcmp_loop:
-	ldx	@tmp2
-	ldab	0,x
-	ldaa	1,x
-	inx
-	inx
-	stx	@tmp2
-	ldx	@tmp3
-	cmpb	0,x
-	bne	_strcmp_ne
-	tstb
-	beq	_strcmp_eq
-	cmpa	1,x
-	bne	_strcmp_ne
-	inx
-	inx
-	stx	@tmp3
-	tsta
-	bne	_strcmp_loop
-_strcmp_eq:
-	clrb
-	clra
-	rts			; return 0
-_strcmp_ne:			; if C==1 return -1 else return 1
-	ldab	#0
-	sbcb	#0		; if C==1 then AccB=$FF else AccB=$00
-	tba
-	orab	#1		; set AccB to $FF or $01
-	rts
+loop:
+	inx			; 4 1
+	inx			; 4 1
+start:
+	stx	@tmp3		; 5 2
+	ldx	@tmp2		; 4 2
+	ldab	0,x		; 5 2
+	beq	s1_end		; 4 2
+	ldaa	1,x		; 5 2
+	beq	s2_end		; 4 2	; ↑ 27cyc
+	inx			; 4 1
+	inx			; 4 1
+	stx	@tmp2		; 5 2
+	ldx	@tmp3		; 4 2
+	subb	0,x		; 5 2
+	bne	ne_b		; 4 2	; ↑ 53cyc
+	suba	1,x		; 5 2
+	beq	loop		; 4 2
+	tab			; 2 1
+ne_b:
+	rora			; 2 1
+	rts			; 5 1
+s1_end:
+	ldx	@tmp3		; 4 2
+	clra			; 2 1
+	subb	0,x		; 5 2
+	rora			; 2 1
+	rts			; 5 1
+s2_end:
+	ldx	@tmp3		; 4 2
+	subb	0,x		; 5 2
+	bne	ne_b		; 4 1
+	suba	1,x		; 5 2
+	rora			; 2 1
+	rts			; 5 1
