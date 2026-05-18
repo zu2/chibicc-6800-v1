cb11708ec85e4a0e10fa754c7f01e9b922fa8d12
zu2 <3023910+zu2@users.noreply.github.com>
2026-05-19 00:14:22 +0900

    update test

--- ztest/0805-isxdigit.c
+++ ztest/0805-isxdigit.c
@@ -1,6 +1,8 @@
-#include "common.h"
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
-int isxdigit(ch)
+int isxdigit_spec(ch)
 {
 	return	(ch>='0' && ch<='9')
 	||	(ch>='A' && ch<='F')
@@ -9,14 +11,25 @@ int isxdigit(ch)
 
 int main(int argc, char **argv)
 {
-	char *p = "Hello, World\n";
+	char s0[] = "Hello, World\n";
+  char s1[20];
+  char s2[20];
+  char *p,*q;
 
-	while (*p){
-		if (isxdigit(*p))
-			putchar(*p);
-		p++;
+	for (p=s0,q=s1; *p; p++ ){
+		if (isxdigit_spec(*p)) {
+      *q = *p;
+      q++;
+    }
 	}
-	putchar('\n');
+  *q = '\0';
+	for (p=s0,q=s2; *p; p++ ){
+		if (isxdigit(*p)) {
+      *q = *p;
+      q++;
+    }
+	}
+  *q = '\0';
 
-	return isxdigit('@');
+	return strcmp(s1,s2);
 }
