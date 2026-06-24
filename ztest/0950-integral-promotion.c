65d10293af5aa3ac3e002868bce2d972f06b070d
zu2 <3023910+zu2@users.noreply.github.com>
2026-06-24 18:34:11 +0900

    add test pattern

--- ztest/0950-integral-promotion.c
+++ ztest/0950-integral-promotion.c
@@ -1,25 +1,44 @@
 int main(void)
 {
-    signed char   a = -1;
-    unsigned char b = 255;
-    int x;
+  signed char   a = -1;
+  unsigned char b = 255;
+  int x;
 
-    if (a==b)      return 1;
-    if (!(a!=b))   return 2;
-    if (!(a < b))  return 3;
-    if (a >= b)    return 4;
-    if (!(b > a))  return 5;
-    if (b <= a)    return 6;
+  if (a==b)      return 1;
+  if (!(a!=b))   return 2;
+  if (!(a < b))  return 3;
+  if (a >= b)    return 4;
+  if (!(b > a))  return 5;
+  if (b <= a)    return 6;
 
-    signed char   c = -128;
-    unsigned char d = 128;
+  signed char   c = -128;
+  unsigned char d = 128;
 
-    if (c == d)    return 7;
-    if (!(c != d)) return 8;
-    if (!(c < d))  return 9;
-    if (c >= d)    return 10;
-    if (!(d > c))  return 11;
-    if (d <= c)    return 12;
+  if (c == d)    return 7;
+  if (!(c != d)) return 8;
+  if (!(c < d))  return 9;
+  if (c >= d)    return 10;
+  if (!(d > c))  return 11;
+  if (d <= c)    return 12;
 
-    return 0;
+  signed char sc;
+  unsigned char uc;
+
+  if ((uc=a&b)!=255) return 21;
+  if ((uc=a|b)!=255) return 22;
+  if ((uc=a^b)!=0)   return 23;
+
+  if ((sc=a&b)!=-1)  return 31;
+  if ((sc=a|b)!=-1)  return 32;
+  if ((sc=a^b)!=0)   return 33;
+
+  if ((uc=c&d)!=128) return 41;
+  if ((uc=c|d)!=128) return 22;
+  if ((uc=c^d)!=0)   return 23;
+
+  if ((sc=c&d)!=-128) return 31;
+  if ((sc=c|d)!=-128) return 32;
+  if ((sc=c^d)!=0)    return 33;
+
+  return 0;
 }
