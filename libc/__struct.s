ddb90aa1711ec86131f77dde80130761fae17388
zu2 <3023910+zu2@users.noreply.github.com>
2026-08-14 06:23:05 +0900

    fix: struct arguments over 8 bytes were pushed backwards
    - __push_struct_x incorrectly pushed the last byte to the lowest address.
    - push_struct() already pushed from the last byte for arguments up to 8 bytes.

--- libc/__struct.s
+++ libc/__struct.s
@@ -1,9 +1,20 @@
 ;
-;   Push AccA,B bytes from 0,x.
+;   Push AccA,B bytes from 0,x, from the last byte to the first,
+;   so the first byte lands at the lowest address.
 ;
 	.export __push_struct_x
 	.code
 __push_struct_x:
+	stab	@tmp2+1
+	staa	@tmp2
+	stx	@tmp1
+	addb	@tmp1+1
+	adca	@tmp1
+	stab	@tmp1+1
+	staa	@tmp1
+	ldx	@tmp1
+	ldab	@tmp2+1
+	ldaa	@tmp2
 	cmpb	#1		; if b!=0, a++
 	sbca	#0
 	inca
@@ -14,9 +25,9 @@ __push_struct_x:
 	pula
 	staa	@tmp1+1
 __loop:
+	dex
 	ldaa	0,x
 	psha
-	inx
 	decb
 	bne	__loop
 	dec	@tmp2
