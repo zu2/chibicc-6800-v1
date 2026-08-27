ea14c29b1f536221d1e1c74fd385b70e62e6a116
zu2 <3023910+zu2@users.noreply.github.com>
2026-08-27 11:30:57 +0900

    libc/__cmp32: call the IX form from the TOS form
    - IX points at TOS, so each TOS entry uses the reversed condition

--- libc/__cmp32.s
+++ libc/__cmp32.s
@@ -52,24 +52,20 @@
 ;
 	.code
 ;
+;	TOS rel @long ?
+;
+;	IX points at TOS, so the IX form takes the reversed condition.
+;
+;
 ;	TOS == @long ?
 ;
 __eq32:
 __eq32s:
 __eq32u:
 	tsx
-	ldx	4,x
-	cpx	@long+2
-	bne	__false
-	tsx
-	ldx	2,x
-	cpx	@long
-	bne	__false
-__true:
-	tsx
-	ldx	0,x
-	clra
-	ldab	#1
+	inx
+	inx
+	jsr	__eq32x
 	bra	__ret
 ;
 ;	TOS != @long ?
@@ -78,31 +74,86 @@ __ne32:
 __ne32s:
 __ne32u:
 	tsx
-	ldx	4,x
-	cpx	@long+2
-	bne	__true
-	tsx
-	ldx	2,x
-	cpx	@long
-	bne	__true
-	bra	__false
+	inx
+	inx
+	jsr	__ne32x
+	bra	__ret
 ;
-;	TOS < @long ?
+;	TOS < (signed) @long ?
 ;
 __lt32s:
-	bsr	__subtl		; TOS - @long
-	blt	__true
-	bra	__false
+	tsx
+	inx
+	inx
+	jsr	__gt32sx
+	bra	__ret
+;
+;	TOS < (unsigned) @long ?
 ;
 __lt32u:
-	bsr	__subtl		; TOS - @long
-	bcs	__true
-__false:
+	tsx
+	inx
+	inx
+	jsr	__gt32ux
+	bra	__ret
+;
+;	TOS > (signed) @long ?
+;
+__gt32s:
+	tsx
+	inx
+	inx
+	jsr	__lt32sx
+	bra	__ret
+;
+;	TOS > (unsigned) @long ?
+;
+__gt32u:
+	tsx
+	inx
+	inx
+	jsr	__lt32ux
+	bra	__ret
+;
+;	TOS <= (signed) @long ?
+;
+__le32s:
+	tsx
+	inx
+	inx
+	jsr	__ge32sx
+	bra	__ret
+;
+;	TOS <= (unsigned) @long ?
+;
+__le32u:
+	tsx
+	inx
+	inx
+	jsr	__ge32ux
+	bra	__ret
+;
+;	TOS >= (signed) @long ?
+;
+__ge32s:
+	tsx
+	inx
+	inx
+	jsr	__le32sx
+	bra	__ret
+;
+;	TOS >= (unsigned) @long ?
+;
+__ge32u:
+	tsx
+	inx
+	inx
+	jsr	__le32ux
+	bra	__ret
+;
+__ret:
 	tsx
 	ldx	0,x
-	clrb
-	clra
-__ret:
 	ins
 	ins
 	ins
@@ -111,64 +162,6 @@ __ret:
 	ins
         tstb                    ; Test AccB for comparison after return
 	jmp	0,x
-__sublt:			; @long - TOS only cc affected
-	tsx
-	ldab	@long+3
-	subb	7,x
-	ldab	@long+2
-	sbcb	6,x
-	ldab	@long+1
-	sbcb	5,x
-	ldab	@long
-	sbcb	4,x
-	rts
-__subtl:			; TOS - @long, only cc affected
-	tsx
-	ldab	7,x
-	subb	@long+3
-	ldab	6,x
-	sbcb	@long+2
-	ldab	5,x
-	sbcb	@long+1
-	ldab	4,x
-	sbcb	@long
-	rts
-;
-;	TOS > @long ?		; @long < TOS ?
-;
-__gt32s:
-	bsr	__sublt		; @long - TOS
-	blt	__true
-	bra	__false
-;
-__gt32u:
-	bsr	__sublt
-	bcs	__true
-	bra	__false
-;
-;	TOS <= @long ? 		; @long >= TOS ?
-;
-__le32s:
-	bsr	__sublt		; @long - TOS
-	bge	__true
-	bra	__false
-;
-__le32u:
-	bsr	__sublt		; @long - TOS
-	bcc	__true
-	bra	__false
-;
-;	TOS >= @long ?
-;
-__ge32s:
-	bsr	__subtl		; TOS - @long
-	bge	__true
-	bra	__false
-;
-__ge32u:
-	bsr	__subtl		; TOS - @long
-	bcc	__true
-	bra	__false
 ;
 ;	32-bit comparison, IX form
 ;
@@ -207,17 +200,13 @@ __eq32bx:
 __eq32dx:
 	jsr	__adx
 __eq32x:
-	ldab	@long+3
-	cmpb	3,x
+	stx	@tmp1
+	ldx	2,x
+	cpx	@long+2
 	bne	__eq32x_0
-	ldab	@long+2
-	cmpb	2,x
-	bne	__eq32x_0
-	ldab	@long+1
-	cmpb	1,x
-	bne	__eq32x_0
-	ldab	@long
-	cmpb	0,x
+	ldx	@tmp1
+	ldx	0,x
+	cpx	@long
 	bne	__eq32x_0
 	clra
 	ldab	#1
@@ -234,17 +223,13 @@ __ne32bx:
 __ne32dx:
 	jsr	__adx
 __ne32x:
-	ldab	@long+3
-	cmpb	3,x
+	stx	@tmp1
+	ldx	2,x
+	cpx	@long+2
 	bne	__ne32x_1
-	ldab	@long+2
-	cmpb	2,x
-	bne	__ne32x_1
-	ldab	@long+1
-	cmpb	1,x
-	bne	__ne32x_1
-	ldab	@long
-	cmpb	0,x
+	ldx	@tmp1
+	ldx	0,x
+	cpx	@long
 	bne	__ne32x_1
 	clra
 	clrb
