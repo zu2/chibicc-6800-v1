c191ee3d1139e5d8f75446febe3c4a3587fed362
zu2 <3023910+zu2@users.noreply.github.com>
2026-08-30 18:54:11 +0900

    gen_direct: add gen_direct_8bit

--- gen_direct.c
+++ gen_direct.c
@@ -1,5 +1,224 @@
 #include "chibicc.h"
 
+static bool gen_direct_8bit_imm_ext_sub(Node *node, char *opb, bool test)
+{
+  int is_store = ((opb!=NULL) && ((strcmp(opb,"stab")==0) || (strcmp(opb,"clr")==0)));
+
+  if (!is_integer_or_ptr(node->ty)) {
+    return 0;
+  }
+
+  switch(node->kind){
+  case ND_NUM:
+    if (is_store) {
+      return 0;
+    }
+    if (test) return 1;
+    if (strcmp(opb,"addb")==0 && (node->val & 0x00ff)==0) {
+      return 1;
+    }
+    if (strcmp(opb,"ldab")==0) {
+      ldab_i((uint16_t)node->val);
+    } else if (strcmp(opb,"andb")==0) {
+      andb_i((uint16_t)node->val);
+    } else if (strcmp(opb,"orab")==0) {
+      orab_i((uint16_t)node->val);
+    } else if (strcmp(opb,"eorb")==0) {
+      eorb_i((uint16_t)node->val);
+    } else {
+      println("\t%s #<%u", opb, (uint16_t)node->val);
+    }
+    return 1;
+  case ND_VAR:
+    if (node->var->ty->kind == TY_VLA) {
+      return 0;
+    }
+    if (node->var->is_local) {
+      return 0;
+    }
+    if (test) return 1;
+    if (node->ty->size == 1) {
+      println("\t%s _%s",opb,node->var->name);
+    } else {
+      println("\t%s _%s+%d",opb,node->var->name,node->ty->size-1);
+    }
+    if (is_store) {
+      invalidate_EXT(node);
+    }
+    return 1;
+  case ND_MEMBER:
+    if (node->member->is_bitfield) {
+      return 0;
+    }
+    if (!is_global_var(node->lhs)) {
+      return 0;
+    }
+    if (test) return 1;
+    println("\t%s _%s+%d",opb,node->lhs->var->name,
+            node->member->offset + node->ty->size-1);
+    if (is_store) {
+      invalidate_EXT(node->lhs);
+    }
+    return 1;
+  case ND_DEREF: {
+    if (node->lhs->kind == ND_NUM) {
+      assert (node->lhs->ty->kind == TY_PTR);
+      if (test) return 1;
+      println("\t%s %ld",opb,node->lhs->val + node->ty->size-1);
+      return 1;
+    }
+    if (node->lhs->kind      == ND_CAST
+    &&  node->lhs->ty->kind  == TY_PTR
+    &&  node->lhs->lhs->kind == ND_NUM
+    &&  is_integer(node->lhs->lhs->ty)) {
+      if (test) return 1;
+      println("\t%s %ld",opb,node->lhs->lhs->val + node->ty->size-1);
+      return 1;
+    }
+    if (node->lhs->kind == ND_VAR
+    &&  is_global_array(node->lhs)) {
+      if (test) return 1;
+      if (node->ty->size == 1) {
+        println("\t%s _%s",opb,node->lhs->var->name);
+      } else {
+        println("\t%s _%s+%d",opb,node->lhs->var->name,node->ty->size-1);
+      }
+      if (is_store) {
+        invalidate_EXT(node->lhs);
+      }
+      return 1;
+    }
+    if (node->lhs->kind == ND_ADD) {
+      Node *lhs = node->lhs->lhs;
+      Node *rhs = node->lhs->rhs;
+      int64_t val;
+      if (lhs->kind == ND_CAST
+      &&  lhs->ty->kind == TY_PTR
+      &&  is_global_array(lhs->lhs)
+      &&  rhs->kind == ND_CAST
+      &&  rhs->ty->kind == TY_PTR
+      &&  is_integer_constant(rhs->lhs,&val)) {
+        if (test) return 1;
+        if (val + node->ty->size-1 == 0) {
+          println("\t%s _%s",opb,lhs->lhs->var->name);
+        } else {
+          println("\t%s _%s+%ld",opb,lhs->lhs->var->name,val + node->ty->size-1);
+        }
+        if (is_store) {
+          invalidate_EXT(lhs->lhs);
+        }
+        return 1;
+      }
+      if (is_global_array(lhs)
+      &&  is_integer_constant(rhs,&val)) {
+        if (test) return 1;
+        if (val + node->ty->size-1 == 0) {
+          println("\t%s _%s",opb,lhs->var->name);
+        } else {
+          println("\t%s _%s+%ld",opb,lhs->var->name,val + node->ty->size-1);
+        }
+        if (is_store) {
+          invalidate_EXT(lhs);
+        }
+        return 1;
+      }
+    }
+    return 0;
+  } // ND_DEREF
+  case ND_CAST:
+    if (node->ty->kind == TY_BOOL) {
+      return 0;
+    }
+    if (is_integer_or_ptr(node->lhs->ty)) {
+      if (test) return can_direct_8bit_imm_ext(node->lhs);
+      return gen_direct_8bit_imm_ext(node->lhs, opb);
+    }
+    return 0;
+  }
+  return 0;
+}
+
+bool can_direct_8bit_imm_ext(Node *rhs)
+{
+  return gen_direct_8bit_imm_ext_sub(rhs,NULL,true);
+}
+
+bool gen_direct_8bit_imm_ext(Node *rhs, char *opb)
+{
+  return gen_direct_8bit_imm_ext_sub(rhs,opb,0);
+}
+
+static bool gen_direct_8bit_ix_sub(Node *node, char *opb, bool test)
+{
+  if (!is_integer_or_ptr(node->ty)) {
+    return 0;
+  }
+
+  switch(node->kind){
+  case ND_NUM:
+    return 0;
+  case ND_VAR: {
+    if (node->var->ty->kind == TY_VLA ) {
+      return 0;
+    }
+    if (!node->var->is_local) {
+      return 0;
+    }
+    if (!test_addr_x(node)) return 0;
+    if (test) return 1;
+    int off = gen_addr_x(node);
+    println("\t%s %d,x",opb,off+node->ty->size-1);
+    return 1;
+  } // ND_VAR
+  case ND_CAST:
+    if (node->ty->kind == TY_BOOL) {
+      return 0;
+    }
+    if (is_integer_or_ptr(node->lhs->ty)) {
+      if (test) return can_direct_8bit_ix(node->lhs);
+      return gen_direct_8bit_ix(node->lhs, opb);
+    }
+    return 0;
+  default:
+    if (test_addr_x(node)) {
+      if (test) return 1;
+      int off = gen_addr_x(node);
+      println("\t%s %d,x",opb,off+node->ty->size-1);
+      return 1;
+    }
+    return 0;
+  }
+  return 0;
+}
+
+bool can_direct_8bit_ix(Node *rhs)
+{
+  return gen_direct_8bit_ix_sub(rhs,NULL,true);
+}
+
+bool gen_direct_8bit_ix(Node *rhs, char *opb)
+{
+  return gen_direct_8bit_ix_sub(rhs,opb,0);
+}
+
+static bool gen_direct_8bit_sub(Node *node, char *opb, bool test)
+{
+  if (gen_direct_8bit_imm_ext_sub(node,opb,test))
+    return 1;
+
+  return gen_direct_8bit_ix_sub(node,opb,test);
+}
+
+bool can_direct_8bit(Node *rhs)
+{
+  return gen_direct_8bit_sub(rhs,NULL,true);
+}
+
+bool gen_direct_8bit(Node *rhs, char *opb)
+{
+  return gen_direct_8bit_sub(rhs,opb,0);
+}
+
 static bool gen_direct_imm_ext_sub(Node *node,char *opb, char *opa, bool test, bool is_char)
 {
   int is_store = ((opb!=NULL) && ((strcmp(opb,"stab")==0) || (strcmp(opb,"clr")==0)));
@@ -339,6 +558,30 @@ static bool gen_direct_imm_ext_sub(Node *node,char *opb, char *opa, bool test, b
   return 0;
 }
 
+bool can_direct_imm_ext(Node *rhs)
+{
+  int r = gen_direct_imm_ext_sub(rhs,NULL,NULL,true,0);
+
+  return r;
+}
+
+bool can_direct_char_imm_ext(Node *rhs)
+{
+  int r = gen_direct_imm_ext_sub(rhs,NULL,NULL,true,1);
+
+  return r;
+}
+
+bool gen_direct_imm_ext(Node *rhs,char *opb, char *opa)
+{
+  return gen_direct_imm_ext_sub(rhs,opb,opa,0,0);
+}
+
+bool gen_direct_char_imm_ext(Node *rhs,char *opb, char *opa)
+{
+  return gen_direct_imm_ext_sub(rhs,opb,opa,0,1);
+}
+
 static bool gen_direct_ix_sub(Node *node,char *opb, char *opa, bool test, bool is_char)
 {
   int is_store = ((opb!=NULL) && ((strcmp(opb,"stab")==0) || (strcmp(opb,"clr")==0)));
@@ -422,6 +665,16 @@ static bool gen_direct_ix_sub(Node *node,char *opb, char *opa, bool test, bool i
   return 0;
 }
 
+bool can_direct_ix(Node *rhs, bool is_char)
+{
+  return gen_direct_ix_sub(rhs,NULL,NULL,true,is_char);
+}
+
+bool gen_direct_ix(Node *rhs, char *opb, char *opa, bool is_char)
+{
+  return gen_direct_ix_sub(rhs,opb,opa,0,is_char);
+}
+
 static bool gen_direct_sub(Node *node,char *opb, char *opa, bool test, bool is_char)
 {
   if (gen_direct_imm_ext_sub(node,opb,opa,test,is_char))
@@ -444,20 +697,6 @@ bool can_direct_char(Node *rhs)
   return r;
 }
 
-bool can_direct_imm_ext(Node *rhs)
-{
-  int r = gen_direct_imm_ext_sub(rhs,NULL,NULL,true,0);
-
-  return r;
-}
-
-bool can_direct_char_imm_ext(Node *rhs)
-{
-  int r = gen_direct_imm_ext_sub(rhs,NULL,NULL,true,1);
-
-  return r;
-}
-
 bool gen_direct(Node *rhs,char *opb, char *opa)
 {
   return gen_direct_sub(rhs,opb,opa,0,0);
@@ -466,16 +705,6 @@ bool gen_direct(Node *rhs,char *opb, char *opa)
 bool gen_direct_char(Node *rhs,char *opb, char *opa)
 {
   return gen_direct_sub(rhs,opb,opa,0,1);
-}
-
-bool gen_direct_imm_ext(Node *rhs,char *opb, char *opa)
-{
-  return gen_direct_imm_ext_sub(rhs,opb,opa,0,0);
-}
-
-bool gen_direct_char_imm_ext(Node *rhs,char *opb, char *opa)
-{
-  return gen_direct_imm_ext_sub(rhs,opb,opa,0,1);
 }
 
 int gen_direct_lr_8bit(Node *node, char *opb)
