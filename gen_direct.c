2d286e996ad731dd933f19af636b36b786043020
zu2 <3023910+zu2@users.noreply.github.com>
2026-08-31 03:16:33 +0900

    gen_direct: skip invalidate_EXT for read-only operations
    - Some paths read invalidate_EXT even when only reading
    - This missed optimization opportunities

--- gen_direct.c
+++ gen_direct.c
@@ -203,8 +203,9 @@ bool gen_direct_8bit_ix(Node *rhs, char *opb)
 
 static bool gen_direct_8bit_sub(Node *node, char *opb, bool test)
 {
-  if (gen_direct_8bit_imm_ext_sub(node,opb,test))
+  if (gen_direct_8bit_imm_ext_sub(node,opb,test)) {
     return 1;
+  }
 
   return gen_direct_8bit_ix_sub(node,opb,test);
 }
@@ -233,8 +234,9 @@ static bool gen_direct_imm_ext_sub(Node *node,char *opb, char *opa, bool test, b
     case TY_ARRAY:
       if (test) return true;
       println("\t%s #<%s", opb, addr);
-      if (opa)
+      if (opa) {
         println("\t%s #>%s",   opa, addr);
+      }
       return 1;
     }
   }
@@ -287,43 +289,45 @@ static bool gen_direct_imm_ext_sub(Node *node,char *opb, char *opa, bool test, b
       if (node->ty->kind==TY_ARRAY) {
         if (test) return true;
         println("\t%s @bp+1",opb);
-        if (opa)
+        if (opa) {
           println("\t%s @bp",opa);
+        }
         if (strcmp(opb,"addb")==0 && node->var->offset==0) {
           return 1;
         }
         println("\t%s #<%d",opb,node->var->offset);
-        if (opa)
+        if (opa) {
           println("\t%s #>%d",opa,node->var->offset);
+        }
         return 1;
       }
     }else{
       // global
-      if (node->ty->kind==TY_FUNC)
-        return 0;
+      if (node->ty->kind==TY_FUNC) return 0;
 //    if (node->ty->kind==TY_CHAR && !node->ty->is_unsigned && !opa)
 //        return 0;
       if (test) return 1;
       if (is_int8(node->ty)) {
-   	    if (is_store) {
-          println("\t%s _%s",opb,node->var->name);
-          invalidate_EXT(node);
-          return 1;
-        }
         println("\t%s _%s",opb,node->var->name);
-        if (opa)
+        if (!is_store && opa) {
           println("\t%s #0",opa);
+        }
+   	    if (is_store) {
+          invalidate_EXT(node);
+        }
 	      return 1;
 	    }
       if (node->ty->kind==TY_ARRAY) {
         println("\t%s #<_%s",opb,node->var->name);
-        if (opa)
+        if (opa) {
           println("\t%s #>_%s",opa,node->var->name);
+        }
 	      return 1;
       }
       println("\t%s _%s+1",opb,node->var->name);
-      if (opa)
+      if (opa) {
         println("\t%s _%s",opa,node->var->name);
+      }
       if (is_store) {
         invalidate_EXT(node);
       }
@@ -347,39 +351,47 @@ static bool gen_direct_imm_ext_sub(Node *node,char *opb, char *opa, bool test, b
       case TY_ENUM:
       case TY_PTR:
         println("\t%s %ld+1",opb,node->lhs->val);
-        if (opa)
+        if (opa) {
           println("\t%s %ld",opa,node->lhs->val);
+        }
         return 1;
       } // ND_DEREF → ND_NUM
       break;
     // (ND_DEREF ty_int (ND_VAR TY_ARRAY(12) _L_1 global)
     case ND_VAR: {
-      if (!is_integer(node->ty) || node->ty->kind==TY_LONG)
+      if (!is_integer(node->ty) || node->ty->kind==TY_LONG) {
         return 0;
+      }
       if (is_global_array(node->lhs)) {
         if (test) return 1;
         switch(node->ty->kind) {
         case TY_BOOL:
         case TY_CHAR:
           println("\t%s _%s",opb,node->lhs->var->name);
-          invalidate_EXT(node->lhs);
+          if (is_store) {
+            invalidate_EXT(node->lhs);
+          }
           return 1;
         case TY_SHORT:
         case TY_INT:
         case TY_ENUM:
         case TY_PTR:
           println("\t%s _%s+1",opb,node->lhs->var->name);
-          if (opa)
+          if (opa) {
             println("\t%s _%s",  opa,node->lhs->var->name);
-          invalidate_EXT(node->lhs);
+          }
+          if (is_store) {
+            invalidate_EXT(node->lhs);
+          }
           return 1;
         }
       }
     } // ND_DEREF → ND_VAR
       break;
     case ND_CAST: {
-      if (!is_integer_or_ptr(node->ty) || node->ty->kind==TY_LONG)
+      if (!is_integer_or_ptr(node->ty) || node->ty->kind==TY_LONG) {
         return 0;
+      }
       if (node->lhs->ty->kind  == TY_PTR
       &&  node->lhs->lhs->kind == ND_NUM
       &&  is_integer(node->lhs->lhs->ty)) {
@@ -394,8 +406,9 @@ static bool gen_direct_imm_ext_sub(Node *node,char *opb, char *opa, bool test, b
         case TY_ENUM:
         case TY_PTR:
           println("\t%s %ld+1",opb,node->lhs->lhs->val);
-          if (opa)
+          if (opa) {
             println("\t%s %ld",  opa,node->lhs->lhs->val);
+          }
           return 1;
         }
       }
@@ -428,12 +441,14 @@ static bool gen_direct_imm_ext_sub(Node *node,char *opb, char *opa, bool test, b
         case TY_PTR:
           if (val==0) {
             println("\t%s _%s+1",opb,lhs->lhs->var->name);
-            if (opa)
+            if (opa) {
               println("\t%s _%s",opa,lhs->lhs->var->name);
+            }
           }else{
             println("\t%s _%s+%ld+1",opb,lhs->lhs->var->name,val);
-            if (opa)
+            if (opa) {
               println("\t%s _%s+%ld",opa,lhs->lhs->var->name,val);
+            }
           }
           return 1;
         }
@@ -457,12 +472,14 @@ static bool gen_direct_imm_ext_sub(Node *node,char *opb, char *opa, bool test, b
           if (test) return 1;
           if (val==0) {
             println("\t%s _%s+1",opb,lhs->var->name);
-            if (opa)
+            if (opa) {
               println("\t%s _%s",  opa,lhs->var->name);
+            }
           }else{
             println("\t%s _%s+%ld+1",opb,lhs->var->name,val);
-            if (opa)
+            if (opa) {
               println("\t%s _%s+%ld",  opa,lhs->var->name,val);
+            }
           }
           return 1;
         }
@@ -488,8 +505,7 @@ static bool gen_direct_imm_ext_sub(Node *node,char *opb, char *opa, bool test, b
     if (node->ty->kind == TY_PTR
     &&  node->lhs->kind == ND_VAR
     &&  is_local_array(node->lhs)) {
-       if (test)
-         return 1;
+       if (test) return 1;
       println("\taddb @bp+1");
       println("\tadca @bp");
       if (node->lhs->var->offset){
@@ -511,8 +527,9 @@ static bool gen_direct_imm_ext_sub(Node *node,char *opb, char *opa, bool test, b
       case TY_ENUM:
       case TY_PTR:
         println("\t%s #<%s", opb,addr);
-        if (opa)
+        if (opa) {
           println("\t%s #>%s", opa,addr);
+        }
         return 1;
       }
       return 0;
@@ -532,10 +549,11 @@ static bool gen_direct_imm_ext_sub(Node *node,char *opb, char *opa, bool test, b
             return is_char || node->ty->is_unsigned;
           }
           println("\t%s _%s+%d",opb,name,moff);
+          if (!is_store && opa) {
+            println("\t%s #0",opa);
+          }
           if (is_store) {
             invalidate_EXT(node->lhs);
-          }else if (opa) {
-            println("\t%s #0",opa);
           }
           return 1;
         case TY_INT:
@@ -544,8 +562,9 @@ static bool gen_direct_imm_ext_sub(Node *node,char *opb, char *opa, bool test, b
         case TY_PTR:
           if (test) return 1;
           println("\t%s _%s+%d",opb,name,moff+1);
-          if (opa)
+          if (opa) {
             println("\t%s _%s+%d",opa,name,moff);
+          }
           if (is_store) {
             invalidate_EXT(node->lhs);
           }
@@ -613,8 +632,9 @@ static bool gen_direct_ix_sub(Node *node,char *opb, char *opa, bool test, bool i
       if (test) return 1;
       int off = gen_addr_x(node);
       println("\t%s %d,x",opb,off+1);
-      if (opa)
+      if (opa) {
         println("\t%s %d,x",opa,off);
+      }
     }
     return 1;
   } // ND_VAR
@@ -653,8 +673,9 @@ static bool gen_direct_ix_sub(Node *node,char *opb, char *opa, bool test, bool i
         if (test) return 1;
         off = gen_addr_x(node);
         println("\t%s %d,x",opb,off+1);
-        if (opa)
+        if (opa) {
           println("\t%s %d,x",opa,off);
+        }
         return 1;
       default:
         return 0;
@@ -672,53 +693,53 @@ bool can_direct_ix(Node *rhs, bool is_char)
 
 bool gen_direct_ix(Node *rhs, char *opb, char *opa, bool is_char)
 {
-  return gen_direct_ix_sub(rhs,opb,opa,0,is_char);
+  return gen_direct_ix_sub(rhs,opb,opa,false,is_char);
 }
 
 static bool gen_direct_sub(Node *node,char *opb, char *opa, bool test, bool is_char)
 {
-  if (gen_direct_imm_ext_sub(node,opb,opa,test,is_char))
-    return 1;
+  if (gen_direct_imm_ext_sub(node,opb,opa,test,is_char)) {
+    return true;
+  }
 
   return gen_direct_ix_sub(node,opb,opa,test,is_char);
 }
 
-bool can_direct(Node *rhs)
-{
-  int r = gen_direct_sub(rhs,NULL,NULL,true,0);
-
-  return r;
-}
-
 bool can_direct_char(Node *rhs)
 {
-  int r = gen_direct_sub(rhs,NULL,NULL,true,1);
-
-  return r;
-}
-
-bool gen_direct(Node *rhs,char *opb, char *opa)
-{
-  return gen_direct_sub(rhs,opb,opa,0,0);
+  return gen_direct_sub(rhs,NULL,NULL,true,1);
 }
 
 bool gen_direct_char(Node *rhs,char *opb, char *opa)
 {
-  return gen_direct_sub(rhs,opb,opa,0,1);
+  return gen_direct_sub(rhs,opb,opa,false,1);
+}
+
+bool can_direct(Node *rhs)
+{
+  return gen_direct_sub(rhs,NULL,NULL,true,0);
+}
+
+
+bool gen_direct(Node *rhs,char *opb, char *opa)
+{
+  return gen_direct_sub(rhs,opb,opa,false,0);
 }
 
 int gen_direct_lr_8bit(Node *node, char *opb)
 {
     if (can_direct_char(node->rhs)){
       gen_expr(node->lhs);
-      if (gen_direct_char(node->rhs,opb,NULL))
+      if (gen_direct_char(node->rhs,opb,NULL)) {
         return 1;
+      }
       assert(0);
     }
     if (can_direct_char(node->lhs)){
       gen_expr(node->rhs);
-      if (gen_direct_char(node->lhs,opb,NULL))
+      if (gen_direct_char(node->lhs,opb,NULL)) {
         return 1;
+      }
       assert(0);
     }
     return 0;
@@ -736,8 +757,9 @@ int gen_direct_lr(Node *node, char *opb, char *opa)
       &&  is_int8(node->rhs->lhs->ty)
       &&  node->rhs->lhs->ty->is_unsigned) {
         gen_expr(node->rhs);
-        if (gen_direct(node->lhs,opb,opa))
+        if (gen_direct(node->lhs,opb,opa)) {
           return 1;
+        }
         assert(0);
       }
     }
@@ -745,14 +767,16 @@ int gen_direct_lr(Node *node, char *opb, char *opa)
 
     if (can_direct_rhs){
       gen_expr(node->lhs);
-      if (gen_direct(node->rhs,opb,opa))
+      if (gen_direct(node->rhs,opb,opa)) {
         return 1;
+      }
       assert(0);
     }
     if (can_direct_lhs){
       gen_expr(node->rhs);
-      if (gen_direct(node->lhs,opb,opa))
+      if (gen_direct(node->lhs,opb,opa)) {
         return 1;
+      }
       assert(0);
     }
     return 0;
