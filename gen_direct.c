4d4b1421d73648b2cb3ded5bd2ec1f9b097c3e24
zu2 <3023910+zu2@users.noreply.github.com>
2026-09-02 22:57:27 +0900

    gen_direct_8bit_ix: accept only types that pass int8()
    - Types larger than one byte are not supported
    - ty->size == 1 also accepts TY_VOID and one-byte structs, which can lead to potential bugs

--- gen_direct.c
+++ gen_direct.c
@@ -242,6 +242,13 @@ bool gen_direct_8bit_imm_ext(Node *node, char *opb)
 
 static bool gen_direct_8bit_ix_sub(Node *node, char *opb, bool test)
 {
+  int is_store = ((opb!=NULL) && ((strncmp(opb,"sta",3)==0)
+                               || (strcmp (opb,"clr")==0)));
+
+  if (is_store && !is_int8(node->ty)) {
+    assert(0);
+  }
+
   if (!is_integer_or_ptr(node->ty)) {
     return false;
   }
