4ffea451eb00f747b9cf765a4d53bab5a935c93a
zu2 <3023910+zu2@users.noreply.github.com>
2026-09-09 21:28:56 +0900

    ast_dump: fix assert failure when ND_NUM has type TY_ENUM

--- ast_dump.c
+++ ast_dump.c
@@ -314,6 +314,7 @@ static void ast_dump(Node *node)
     case TY_CHAR:
     case TY_SHORT:
     case TY_LONG:
+    case TY_ENUM:
       printout("(ND_NUM %s %ld)", type_str(node->ty),node->val);
       break;
     case TY_INT:
