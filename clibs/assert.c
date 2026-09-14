e2afac43224ec9ee35d36446b1c71b310c473148
zu2 <3023910+zu2@users.noreply.github.com>
2026-09-15 04:55:45 +0900

    clibs/assert.c: Fix the number of arguments passed to printf

--- clibs/assert.c
+++ clibs/assert.c
@@ -2,9 +2,7 @@
 #include <stdlib.h>
 
 void __assert_fail(const char *expr, const char *file, int line, const char *func) {
-    const char *s = func ? func : "";
-    printf("%s:%d: %s%s%s: Assertion `%s' failed.\n",
-        file, line, s, *s ? ": " : "", expr);
+    printf("%s:%d: %s: Assertion `%s' failed.\n", file, line, func, expr);
     asm("\tldab #134");
     abort();
 }
