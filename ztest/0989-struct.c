36f438dad5e5f3cb893d0165785ee825feb7d8b5
zu2 <3023910+zu2@users.noreply.github.com>
2026-09-26 02:52:42 +0900

    0989-struct.c: update test

--- ztest/0989-struct.c
+++ ztest/0989-struct.c
@@ -1,4 +1,4 @@
-union overlap {
+union overlap_long {
   long y;
   struct {
     char pad;
@@ -6,13 +6,62 @@ union overlap {
   } a;
 };
 
-union overlap u;
+union overlap_float {
+  float y;
+  struct {
+    char pad;
+    float x;
+  } a;
+};
 
-int
-main(int argc, char **atgv)
+struct four_bytes {
+  char b[4];
+};
+
+union overlap_struct {
+  struct four_bytes y;
+  struct {
+    char pad;
+    struct four_bytes x;
+  } a;
+};
+
+union overlap_long ul;
+union overlap_float uf;
+union overlap_struct us;
+
+static int test_long(void) {
+  ul.a.x = 0x12345678L;
+  ul.y = ul.a.x;
+  return ul.y != 0x12345678L;
+}
+
+static int test_float(void) {
+  uf.a.x = 1.0f;
+  uf.y = uf.a.x;
+  return uf.y != 1.0f;
+}
+
+static int test_struct(void) {
+  us.y.b[0] = 1;
+  us.y.b[1] = 2;
+  us.y.b[2] = 3;
+  us.y.b[3] = 4;
+  us.a.x = us.y;
+  return us.a.x.b[0] != 1 || us.a.x.b[1] != 2 ||
+         us.a.x.b[2] != 3 || us.a.x.b[3] != 4;
+}
+
+int main(int argc, char **argv)
 {
-  u.a.x = 0x12345678L;
-  u.y = u.a.x;
+  if (test_long())
+    return 1;
 
-  return u.y != 0x12345678L;
+  if (test_float())
+    return 2;
+
+  if (test_struct())
+    return 3;
+
+  return 0;
 }
